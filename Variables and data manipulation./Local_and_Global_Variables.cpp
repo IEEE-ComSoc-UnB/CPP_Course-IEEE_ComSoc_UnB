8c1f41f90e9ea1c7e2a7d9f4cd267079fa23628c
Bruno Vieira Dutra <bruno.dutra@uiot.org>
2017-06-18 14:00:21 -0300

    Update Local_and_Global_Variables.cpp

--- Variables and data manipulation./Local_and_Global_Variables.cpp
+++ Variables and data manipulation./Local_and_Global_Variables.cpp
@@ -7,14 +7,12 @@ int a = 45;
 int main()
 {
 	int a = 100;
-    cout << "Valor global de A: "<< ::a << endl;
-    cout << "Valor local de A: "<< a << endl;
-    
-    printf("Valor global de A: %d", ::a);
-    printf("Valor global de A: %d",   a);
-    
-    
-  
-    
-    return 0;       
+    	cout << "Valor global de A: "<< ::a << endl;
+    	cout << "Valor local de A: "<< a << endl;
+	
+    	//outra forma
+    	
+	printf("Valor global de A: %d", ::a);
+    	printf("Valor local de A: %d" ,   a);
+    	return 0;       
 }
